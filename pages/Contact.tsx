7591fcee00da631c2b88af5e737d839e50c4cec0
Tshidiso Donald Mzima <donaldmzima8@gmail.com>
2022-12-04 23:02:17 +0200

    feat/adding-pages

---
+++ pages/Contact.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Form } from '../components/Form'
+
+const Contact = () => {
+  return (
+    <>
+      {' '}
+      <div>Contact</div>
+      <div>
+        {' '}
+        <Form />
+      </div>
+    </>
+  )
+}
+
+export default Contact
