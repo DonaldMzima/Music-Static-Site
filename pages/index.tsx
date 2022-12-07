d69b0b5179457e74b364605b5b783e92d8720539
Tshidiso Donald Mzima <donaldmzima8@gmail.com>
2022-12-07 21:07:55 +0200

    add/landing-page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
-import { Box, Img } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Heading,
+  Stack,
+  Text,
+} from '@chakra-ui/react'
 import Head from 'next/head'
 import Image from 'next/image'
 import Footer from '../components/footer/Footer'
 import NavBar from '../components/NavBar'
 
 import styles from '../styles/Home.module.css'
+import SvgComponent from '../svgs/SVG'
 
 export default function Home() {
   return (
@@ -12,13 +21,50 @@ export default function Home() {
       <div>
         <NavBar />
 
-        <Img
-          boxSize="750px"
-          objectFit="cover"
-          src={
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrPo9lVrXxqAnarF0j67287aVJtmO8oMc6Iw&usqp=CAU'
-          }
-        />
+        <Container maxW={'5xl'}>
+          <Stack
+            textAlign={'center'}
+            align={'center'}
+            spacing={{ base: 8, md: 10 }}
+            py={{ base: 20, md: 28 }}
+          >
+            <Heading
+              fontWeight={600}
+              fontSize={{ base: '3xl', sm: '4xl', md: '6xl' }}
+              lineHeight={'110%'}
+            >
+              Meeting scheduling{' '}
+              <Text as={'span'} color={'orange.400'}>
+                made easy
+              </Text>
+            </Heading>
+            <Text color={'gray.500'} maxW={'3xl'}>
+              Never miss a meeting. Never be late for one too. Keep track of
+              your meetings and receive smart reminders in appropriate times.
+              Read your smart “Daily Agenda” every morning.
+            </Text>
+            <Stack spacing={6} direction={'row'}>
+              <Button
+                rounded={'full'}
+                px={6}
+                colorScheme={'orange'}
+                bg={'orange.400'}
+                _hover={{ bg: 'orange.500' }}
+              >
+                Get started
+              </Button>
+              <Button rounded={'full'} px={6}>
+                Learn more
+              </Button>
+            </Stack>
+            <Flex w={'full'}>
+              <SvgComponent
+                height={{ sm: '24rem', lg: '28rem' }}
+                mt={{ base: 12, sm: 16 }}
+              />
+            </Flex>
+          </Stack>
+        </Container>
 
         <Footer />
       </div>
