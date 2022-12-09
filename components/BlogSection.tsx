7f76121933ebd3da47cf0f3a319034303c7d2516
Tshidiso Donald Mzima <donaldmzima8@gmail.com>
2022-12-10 00:02:19 +0200

    adding/blog/tittle

--- components/BlogSection.tsx
+++ components/BlogSection.tsx
@@ -125,6 +125,13 @@ const ArticleList = () => {
             Houston bowling alley. He was 28.
           </Text>
         </Box>
+        <Text>
+          Police arrested two people in connection with the slaying of renowned
+          Migos rapper Takeoff, who was an innocent bystander when he was
+          fatally shot last month, Houston authorities said Friday. Patrick
+          Clark, 33, was arrested Thursday on a charge of murder, Houston Police
+          Chief Troy Finner said in a news conference.
+        </Text>
       </Box>
     </Container>
   )
