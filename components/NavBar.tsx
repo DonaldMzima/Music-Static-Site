43061d4f0e6c89c078204b6db657f15d4743ef6e
Tshidiso Donald Mzima <90970069+DonaldMzima@users.noreply.github.com>
2023-05-03 15:43:47 +0200

    Revert "fix:fix the routing"

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -74,22 +74,6 @@ const Navbar = () => {
           </Button>
         </Link>
 
-
-              <Link href={''}>
-                <Button
-                  variant="ghost"
-                  as="a"
-                  aria-label="Contact"
-                  my={5}
-                  w="100%"
-                  onClick={() => route.push('/contact')}
-                >
-                  Contact us
-                </Button>
-              </Link>
-            </Stack>
-          </Container>
-
         <Link>
           <Button
             variant="ghost"
@@ -103,8 +87,11 @@ const Navbar = () => {
           </Button>
         </Link>
 
-
-       
+        <Link href="/contact">
+          <Button variant="ghost" as="a" aria-label="Contact" my={5} w="100%">
+            Contact us
+          </Button>
+        </Link>
       </Flex>
       <Box>
         <DarkMode />
