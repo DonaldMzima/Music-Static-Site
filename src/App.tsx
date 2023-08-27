2f5571cf2c47957942766bd8916768d2bb4e115b
Tshidiso Donald Mzima <donaldmzima8@gmail.com>
2023-08-27 20:51:34 +0200

    add log

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
 
 import './App.css'
+import Simple from './components/bar'
 import { Form } from './components/Form'
-import NavBar from './components/navBar/Nav'
+import SmallCentered from './components/navBar/Nav'
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <NavBar />
-      </div>
+      <Simple />
       <Form />
     </div>
   )
